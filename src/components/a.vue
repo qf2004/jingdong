01ed4fad7c2c3a8cc3514c88fb745aaa01f128e0
tianwater <7701037@qq.com>
2020-09-16 14:59:33 +0800

    a.vue

---
+++ src/components/a.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    a 组件
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Banner",
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
